cddc89a6e6f688e216a1fd535ae16257ccb6d88c
Meet <meetvk57@gmail.com>
2023-12-01 22:57:11 -0500

    sqlx and postgress

--- SimpleBackend/src/main.rs
+++ SimpleBackend/src/main.rs
@@ -1,8 +1,24 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use sqlx::postgres::{PgPoolOptions, PgRow};
+use sqlx::{FromRow, Row, Pool, Postgres};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    dotenv().ok();
+    
+
+    let postgres_url = std::env::var("DATABASE_URL").expect("Must set up DB URL");
+
+    let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&postgres_url)
+    .await.expect("DB Failed to connect");
+
+    init_tables(&pool).await.expect("Failed to initialize tables");
+
+
     HttpServer::new(
         ||
         {
@@ -18,4 +34,14 @@ async fn main() -> std::io::Result<()> {
 #[get("/")]
 async fn main_page() -> impl Responder {
     HttpResponse::Ok().body("Hello")
+}
+
+async fn init_tables(pool: &Pool<Postgres>) -> Result<(), sqlx::Error> {
+    sqlx::query(r#"
+    CREATE TABLE IF NOT EXISTS test_table (
+        id bigserial,
+        info text
+    );"#,).execute(pool).await?;
+
+    return Ok(());
 }
