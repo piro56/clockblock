53e569c6fe38190507de9de11f518dbcfbb4eb6a
Meet <meetvk57@gmail.com>
2023-12-01 21:59:58 -0500

    actix set up

--- SimpleBackend/src/main.rs
+++ SimpleBackend/src/main.rs
@@ -1,6 +1,21 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-
-
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    HttpServer::new(
+        ||
+        {
+            let mut app = App::new();
+            app = app.service(main_page);
+            return app;
+        }
+    ).bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn main_page() -> impl Responder {
+    HttpResponse::Ok().body("Hello")
 }
