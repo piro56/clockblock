0e3fadbd240603dacc686d3d217786455cbcb6d8
piro56 <meetvk57@gmail.com>
2023-12-06 18:29:07 -0500

    time conversion

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,35 @@
 "use client"
 import DatePicker from "react-datepicker";
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import "react-datepicker/dist/react-datepicker.css";
 import "./datepicker.css";
 
 
 export default function Home() {
   const [startDate, setStartDate] = useState(new Date());
+  const [selectorToggle, setSelector] = useState(false);
+  const [selectTimestamp, setTimestamp] = useState(0);
+  const [timeInput, setTimeInput] = useState("");
 
+  function onDatePick(date: Date | null) {
+    if (!date) return;
+    setStartDate(date);
+    setTimestamp(date.getUTCDate());
+    setTimeInput((date.getTime()/1000).toString());
+  }
+  function onTimestampPick(s: string) {
+    let time = parseInt(s);
+    time = time * 1000;
+    if (isNaN(time)) {
+      console.log("IsNan.");
+      return;
+    }
+    let dateTime = new Date(time);
+    setTimestamp(time);
+    setStartDate(dateTime);
+    setTimeInput(s);
+    console.log(dateTime);
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between justify-items-center pl-24 pr-24 pt-12">
@@ -27,11 +49,20 @@ export default function Home() {
           <div className="flex flex-row grow align-center justify-items-center p-2 gap-2">
             <div className="flex flex-col hidden md:flex justify-center grow text-center bg-gray-800 rounded"
              title="You will not be able to retrieve your key (and nonce) until after this timestamp.">
-              <h1>Unlock Time:</h1>
+              <button onClick={(e) => {setSelector(!selectorToggle);}}>{selectorToggle ? "Unlock Date:" : "Unlock Timestamp:"}</button>
             </div>
-            <form className='grow min-h-[40px] '>
-              <DatePicker dateFormat="MM/dd/yyyy HH:mm" selected={startDate} onChange={ (date) => {if (date) setStartDate(date)}} showTimeSelect/>
-            </form>
+            { selectorToggle && 
+              <form className='grow min-h-[40px] '>
+                <DatePicker dateFormat="MM/dd/yyyy HH:mm" selected={startDate} onChange={(date) => onDatePick(date)} showTimeSelect/>
+              </form>
+            }
+            {
+              !selectorToggle &&
+              <form className='grow min-h-[40px] '>
+                <input className='text-center bg-gray-800 rounded-md w-[100%] min-h-[40px]' placeholder='Timestamp' 
+                        onInput={(e: any) => onTimestampPick(e.target.value)} value={timeInput}></input>
+              </form>
+            }
           </div>
 
           <form className='basis-full'>
